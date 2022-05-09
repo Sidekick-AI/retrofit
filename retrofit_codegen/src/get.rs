9b547730f6de5831c91c280eb515d96c8ccb2eb1
Joe Fioti <jafioti@gmail.com>
2022-05-08 22:32:13 -0400

    Added security to get api

--- retrofit_codegen/src/get.rs
+++ retrofit_codegen/src/get.rs
@@ -1,4 +1,7 @@
+use std::{collections::hash_map::DefaultHasher, hash::{Hash, Hasher}};
+
 use proc_macro::TokenStream;
+use rand::{prelude::StdRng, distributions::Alphanumeric, SeedableRng, Rng};
 use syn::{parse_macro_input, ItemFn, Type, FnArg, PatType, Pat, PatIdent};
 use quote::{quote, ToTokens};
 use proc_macro2::{Ident, Span};
@@ -33,6 +36,11 @@ pub fn get_api(header: TokenStream, function: TokenStream) -> TokenStream {
         args.pop();
     }
     let arg_idents: Vec<Ident> = raw_args.iter().map(|i| i.ident.clone()).collect();
+    // Ensure no args are vecs
+    for arg in &args {
+        assert!(!arg.into_token_stream().to_string().to_lowercase().contains("vec"), "Vec arguments cannot be used in get apis. Try using a post api instead.")
+    }
+
     let arg_types: Vec<(Ident, proc_macro2::TokenTree, bool)> = args.iter().zip(arg_idents.iter()).map(|(i, ident)| 
         (
             ident.clone(),
@@ -61,6 +69,7 @@ pub fn get_api(header: TokenStream, function: TokenStream) -> TokenStream {
         let ident = arg.ident.clone();
         quote! {serde_json::to_string( & #ident.clone() ).unwrap()}
     }).collect();
+
     // Reqwest cannot take relative urls so we put localhost (the base url should be an option defined in the macro)
     let request_path = if request_path_strings.is_empty() {
         quote!{&format!("http://localhost:8000/{}", #input_fn_ident_string)} 
@@ -74,10 +83,44 @@ pub fn get_api(header: TokenStream, function: TokenStream) -> TokenStream {
         quote!{&format!("/{}/{}", #input_fn_ident_string, [#(#request_path_strings),*].join("/"))}
     };
 
+    // Security type and random string
+    let secure_struct_ident = Ident::new(&format!("{}Secure", input_fn_ident_string), Span::call_site());
+    let mut s = DefaultHasher::new();
+    input_fn_ident_string.hash(&mut s);
+    let hash = s.finish();
+    let secure_string = format!("Bearer {}", StdRng::seed_from_u64(hash)
+        .sample_iter(&Alphanumeric)
+        .take(10)
+        .map(char::from)
+        .collect::<String>());
+
     let route_ident = Ident::new(&format!("{}_route", input_fn_ident_string), Span::call_site());
     let request_ident = Ident::new(&format!("{}_request", input_fn_ident_string), Span::call_site());
 
     TokenStream::from(quote!{
+        // Secure Struct
+        #[derive(Debug)]
+        pub struct #secure_struct_ident;
+
+        #[cfg(feature = "server")]
+        #[rocket::async_trait]
+        impl<'r> rocket::request::FromRequest<'r> for #secure_struct_ident {
+            type Error = String;
+
+            async fn from_request(req: &'r rocket::Request<'_>) -> rocket::request::Outcome<Self, Self::Error> {
+                match req.headers().get_one("authorization") {
+                    Some(h) => {
+                        if h == #secure_string { // Random string that will be in the header of every request
+                            rocket::request::Outcome::Success(#secure_struct_ident)
+                        } else {
+                            rocket::request::Outcome::Forward(())
+                        }
+                    }
+                    None => rocket::request::Outcome::Forward(()),
+                }
+            }
+        }
+
         // Original function
         #[cfg(feature = "server")]
         #[allow(clippy::ptr_arg)]
@@ -86,7 +129,7 @@ pub fn get_api(header: TokenStream, function: TokenStream) -> TokenStream {
         // Route function
         #[cfg(feature = "server")]
         #[rocket::get(#route_path)]
-        pub fn #route_ident ( #(#arg_idents : String),* #state) -> String {
+        pub fn #route_ident ( #(#arg_idents : String),* #state, _secure: #secure_struct_ident) -> String {
             serde_json::to_string(& #input_fn_ident ( #(#fn_input_args),* #pass_through_state)).unwrap()
         }
 
@@ -97,13 +140,16 @@ pub fn get_api(header: TokenStream, function: TokenStream) -> TokenStream {
             // Send request to endpoint
             #[cfg(not(target_family = "wasm"))]
             return serde_json::from_str(
-                &reqwest::get(#request_path)
-                .await.unwrap()
+                &reqwest::Client::new()
+                .get(#request_path)
+                .header("authorization", #secure_string)
+                .send().await.unwrap()
                 .text().await.unwrap()
             ).unwrap();
 
             #[cfg(target_family = "wasm")]
             return reqwasm::http::Request::get(#reqwasm_request_path)
+                .header("authorization", #secure_string)
                 .send().await.unwrap()
                 .json().await.unwrap();
         }
